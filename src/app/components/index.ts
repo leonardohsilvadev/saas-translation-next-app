2126ecebae45be422d1b7b839b85b45e68ab5fd4
Leonardo Silva <leonardohsilva.dev@gmail.com>
2023-11-13 15:43:43 -0300

    feat: add ThemeProvider Component

---
+++ src/app/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Header } from './Header'
+export { default as Logo } from './Logo'
+export { default as DarkModeToggle } from './DarkModeToggle'
+export { default as ThemeProvider } from './ThemeProvider'
