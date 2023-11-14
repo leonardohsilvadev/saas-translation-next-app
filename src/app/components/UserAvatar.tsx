f80ec66dc080f8adea411b0c6bd8d8bb54032b5c
Leonardo Silva <leonardohsilva.dev@gmail.com>
2023-11-14 08:22:35 -0300

    feat: add UserAvatar component

---
+++ src/app/components/UserAvatar.tsx
@@ -0,0 +1,40 @@
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/components/ui/avatar"
+import { cn } from "@/app/lib/utils"
+import Image from "next/image"
+
+type Props = {
+  name: string,
+  image: string,
+  className?: string
+}
+
+export default function UserAvatar({
+  name,
+  image,
+  className
+}: Props) {
+  return (
+    <Avatar className={cn('bg-white text-black', className)}>
+      {image && (
+        <Image
+          src={image}
+          alt={name}
+          width={40}
+          height={40}
+          className="rounded-full"
+        />
+      )}
+
+      <AvatarFallback
+        className="dark:bg-white dark:text-black text-lg"
+        delayMs={1000}
+      >
+        {name?.split(' ').map((n) => n[0]).join('')}
+      </AvatarFallback>
+    </Avatar>
+  )
+}
