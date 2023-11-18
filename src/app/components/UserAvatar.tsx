4c0f1d5b909468d2deb51f30c89b17c39a5c3191
Leonardo Silva <leonardohsilva.dev@gmail.com>
2023-11-18 14:57:33 -0300

    feat: add UserButton n UserAvatar components

--- src/app/components/UserAvatar.tsx
+++ src/app/components/UserAvatar.tsx
@@ -1,28 +1,20 @@
-import {
-  Avatar,
-  AvatarFallback,
-  AvatarImage,
-} from "@/components/ui/avatar"
-import { cn } from "@/app/lib/utils"
-import Image from "next/image"
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { cn } from "@/app/lib/utils";
+import Image from "next/image";
 
 type Props = {
-  name: string,
-  image: string,
-  className?: string
-}
+  name: string | null | undefined;
+  image: string | null | undefined;
+  className?: string;
+};
 
-export default function UserAvatar({
-  name,
-  image,
-  className
-}: Props) {
+export default function UserAvatar({ name, image, className }: Props) {
   return (
-    <Avatar className={cn('bg-white text-black', className)}>
+    <Avatar className={cn("bg-white text-black", className)}>
       {image && (
         <Image
           src={image}
-          alt={name}
+          alt={name ?? ""}
           width={40}
           height={40}
           className="rounded-full"
@@ -33,8 +25,11 @@ export default function UserAvatar({
         className="dark:bg-white dark:text-black text-lg"
         delayMs={1000}
       >
-        {name?.split(' ').map((n) => n[0]).join('')}
+        {name
+          ?.split(" ")
+          .map((n) => n[0])
+          .join("")}
       </AvatarFallback>
     </Avatar>
-  )
+  );
 }
---
+++ src/app/components/UserButton.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { UserAvatar } from ".";
+import { Session } from "next-auth";
+import { Button } from "./ui/button";
+import { signIn, signOut } from "next-auth/react";
+
+function UserButton({ session }: { session: Session | null }) {
+  if (!session)
+    return (
+      <Button variant="outline" onClick={() => signIn()}>
+        Sign In
+      </Button>
+    );
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <UserAvatar name={session.user?.name} image={session.user?.image} />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>{session.user?.name}</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={() => signOut()}>Sign Out</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+
+export default UserButton;
